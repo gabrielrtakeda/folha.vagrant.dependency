3d74677253814e888b6a14791a3f11bc8abae79d
Gabriel Ramos Takeda <gabrieel.rt@gmail.com>
2015-05-18 20:10:21 -0300

    Setar módulos ativos na compilação do Apache.

--- setup-apache.sh
+++ setup-apache.sh
@@ -2,4 +2,17 @@
 DIST='2.2.23'
 VERSAO=`date +"%Y%m%d-%H%M"`
 ./configure --prefix=/usr/local/apache-$DIST-$VERSAO \
-
+--enable-deflate=shared \
+--enable-expires \
+--enable-headers \
+--enable-proxy-ajp \
+--enable-proxy-balancer \
+--enable-proxy-connect \
+--enable-proxy-ftp \
+--enable-proxy-http \
+--enable-proxy \
+--enable-proxy-scgi \
+--enable-rewrite \
+--enable-so \
+--enable-ssl \
+--with-included-apr
