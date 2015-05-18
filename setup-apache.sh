52cee52e1011849c1886ce4fe622d160b65af871
Gabriel Ramos Takeda <gabrieel.rt@gmail.com>
2015-05-18 17:37:30 -0300

    Adicionar shell de configuração: Apache & PHP

---
+++ setup-apache.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DIST='2.2.23'
+VERSAO=`date +"%Y%m%d-%H%M"`
+./configure --prefix=/usr/local/apache-$DIST-$VERSAO \
+
---
+++ setup-php.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+DIST='5.3.20'
+VERSAO=`date +"%Y%m%d-%H%M"`
+./configure --prefix=/usr/local/php-$DIST-backend-$VERSAO \
+--with-apxs2=/usr/local/apache2/bin/apxs \
+--with-oci8=instantclient,/usr/local/instantclient_10_2/ \
+--enable-bcmath \
+--enable-mbstring \
+--enable-soap \
+--enable-gd-native-ttf \
+--enable-exif \
+--with-zlib=yes \
+--with-mssql=/usr/local/freetds-0.82 \
+--with-config-file-path=/etc/ \
+--with-mysql=/usr \
+--with-config-file-path=/etc \
+--with-xmlrpc&nbsp; \
+--with-gd=/usr \
+--with-openssl \
+--with-jpeg-dir=/usr \
+--with-png-dir=/usr \
+--with-iconv \
+--with-freetype-dir=/usr/include/freetype2/freetype \
+--with-xpm-dir=/usr/X11R6 \
+--with-curl=/usr \
+--enable-zip \
+--with-ldap \
+--with-pspell=/usr \
+--with-xsl=/lusr/local/lib \
+--with-mysqli=mysqlnd
+
+rm -rf /root/.pearrc
